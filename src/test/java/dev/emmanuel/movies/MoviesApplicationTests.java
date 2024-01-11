bbca9eab4adc79c4ed89172ceb8411740d484562
so2emma <osoemmanuel1969@gmail.com>
2024-01-11 15:09:09 +0100

    first commit

---
+++ src/test/java/dev/emmanuel/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.emmanuel.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
